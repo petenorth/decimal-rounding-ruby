ee810fc9f3df5b06e429c14cf857a7f0d2c11400
Peter Fry <3152937+petenorth@users.noreply.github.com>
2020-03-06 14:00:54 +0000

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,23 +1,23 @@
 require 'bundler/setup'
 require 'active_record'
 
-class Thing < ActiveRecord::Base
-end
+#class Thing < ActiveRecord::Base
+#end
 
-ActiveRecord::Base.establish_connection
+#ActiveRecord::Base.establish_connection
 
-ActiveRecord::Base.connection.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS things (
-    fee DECIMAL(6, 2)
-  )
-SQL
+#ActiveRecord::Base.connection.execute <<-SQL
+#  CREATE TABLE IF NOT EXISTS things (
+#    fee DECIMAL(6, 2)
+#  )
+#SQL
 
-thing = Thing.new(fee: 444.44)
+#thing = Thing.new(fee: 444.44)
 expected = BigDecimal.new('444.44')
-actual = thing.fee
-delta = actual - expected
+#actual = thing.fee
+#delta = actual - expected
 
 puts 'Expected    %s' % expected.inspect
-puts 'But got     %s' % actual.inspect
-puts 'Difference  %s' % delta.inspect
+#puts 'But got     %s' % actual.inspect
+#puts 'Difference  %s' % delta.inspect
 
